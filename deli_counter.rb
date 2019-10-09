670fe811b08b4085f0f8721b857fe6bd8ea93a20
Huda Yousif <hjdoski@gmail.com>
2019-10-09 21:34:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,15 @@
 katz_deli = []
 
-def line(empty)
-  puts "The line is currenly empty."
-end 
+def line(array)
+ current_line = []
+   if current_line.size == 0
+     puts "The line is currently empty."
+   else
+     array.each do |name|
+       customers = "The line is currently: #{num}. #{name}"
+       num += 1
+       current_line << customers
+   end
+     return current_line
+ end
+end
