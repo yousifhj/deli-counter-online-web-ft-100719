03e9b0b7a30ee2cdcf5af3ce28219a96a6d3d16d
Huda Yousif <hjdoski@gmail.com>
2019-10-09 21:39:56 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,8 @@
+katz_deli = [] 
+
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end 
