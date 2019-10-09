48edfa7f364bda4b8d69e249d90f2e8aa4a28307
Huda Yousif <hjdoski@gmail.com>
2019-10-09 21:37:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,3 +8,21 @@ function nowServing(katzDeli) {
   }
   else
   return (`Currently serving ${katzDeli.shift()}.`);
+  var line = [];
+  
+
+
+
+
+
+function currentLine(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
+}
