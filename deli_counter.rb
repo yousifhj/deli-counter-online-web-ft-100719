0bba54a136a8a813a4ba07e615aeacc90ce7cb65
Huda Yousif <hjdoski@gmail.com>
2019-10-09 21:24:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,6 @@
 var katzDeli = []
 
-def
-
-def take_a_number()
+function takeANumber(katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
