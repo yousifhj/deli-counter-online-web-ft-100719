929102eb21c51773433d159bebb8ff2eff304c28
Huda Yousif <hjdoski@gmail.com>
2019-10-09 21:38:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,22 +1,7 @@
+var katzDeli = []; 
 
-  var line = [];
-  
-
-
-
-
-
-function currentLine(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
+function takeANumber(katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
-
-end 
  
